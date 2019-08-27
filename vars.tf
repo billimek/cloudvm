66909fcf624fff2da0eab72c05ffb5cabeac5996
Jeff Billimek <jeffrey_k_billimek@homedepot.com>
2019-08-27 17:52:15 -0400

    add necessary descriptions

--- vars.tf
+++ vars.tf
@@ -1,9 +1,11 @@
 variable "project" {
   type = string
+  description = "The name of your google-cloud project, e.g. 'free-micro-123456'"
 }
 
 variable "credentials" {
   type = string
+  description = "The location of a google cloud credentials json file, e.g. '~/.config/gcloud/legacy_credentials/me@domain.com/adc.json'"
 }
 
 variable "region" {
